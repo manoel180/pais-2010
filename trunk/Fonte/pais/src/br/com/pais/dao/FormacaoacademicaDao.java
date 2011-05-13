c3bfca461bb6340c6dfbbbe10839fa8dad12083a
manoel180@gmail.com <manoel180@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-05-13 20:11:43 +0000

    excluido para adiconar no projeto pais_model

--- trunk/Fonte/pais/src/br/com/pais/dao/FormacaoacademicaDao.java
+++
@@ -1,8 +0,0 @@
-package br.com.pais.dao;
-
-import br.com.pais.entities.Formacaoacademica;
-
-
-public interface FormacaoacademicaDao extends DaoGenerico<Formacaoacademica, Integer> {
-	
-}
--- trunk/Fonte/pais/src/br/com/pais/dao/TipoBasesDao.java
+++
@@ -1,8 +0,0 @@
-package br.com.pais.dao;
-
-import br.com.pais.entities.Tipobases;
-
-
-public interface TipoBasesDao extends DaoGenerico<Tipobases, Integer> {
-
-}
