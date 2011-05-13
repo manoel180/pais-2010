34bcfa7ffd3483d89a88162a374eddb9633b34a8
andre.nascimento18@gmail.com <andre.nascimento18@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-05-13 15:35:53 +0000

    atualização

--- Fonte/pais/src/br/com/pais/dao/BasesDao.java
+++ Fonte/pais/src/br/com/pais/dao/BasesDao.java
@@ -8,5 +8,5 @@ import br.com.pais.entities.Bases;
 public interface BasesDao extends DaoGenerico<Bases, Integer> {
 
 	public List<Bases> listarBases(int discipulador);
-	
+	public List<Bases> listarBasesPorTipo(int discipulador, int tipo);
 }
--- Fonte/pais/src/br/com/pais/dao/RepasseDao.java
+++ Fonte/pais/src/br/com/pais/dao/RepasseDao.java
@@ -7,21 +7,37 @@ import br.com.pais.entities.Repasse;
 
 public interface RepasseDao extends DaoGenerico<Repasse,Integer> {
 	
-	//Receber Movimentos
-	List<Movimento> listarMovimentosReceber(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
-	List<Object> totalMovimentosReceber(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
-	List<Movimento> listarMovimentosTodosReceber(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
-	//Enviados
-	List<Movimento> listarMovimentosEnviados(int lider, int geracao, String tipo, String especie, String DataInicio, String DataFim);
-	List<Object> totalMovimentosEnviados(int celula, String tipo, String especie, String DataInicio, String DataFim);
-	List<Movimento> listarMovimentosTodosEnviados(int celula, String tipo, String especie, String DataInicio, String DataFim);
-	//Recebidos
-	List<Movimento> listarMovimentosRecebdidos(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
-	List<Object> totalMovimentosRecebidos(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
-	List<Movimento> listarMovimentosTodosRecebidos(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	/*-------------------------------------------CELULAS Á RECEBER--------------------------------------------------------*/
+	List<Movimento> listarMovimentosReceberCelulas(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosReceberCelulas(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosReceberCelulas(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	/*-------------------------------------------BASES Á RECEBER--------------------------------------------------------*/
+	List<Movimento> listarMovimentosReceberBases(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosReceberBases(int base, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosReceberBases(int base, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	
+	/*-------------------------------------------CELULAS RECEBIDOS--------------------------------------------------------*/
+	List<Movimento> listarMovimentosRecebidosCelulas(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosRecebidosCelulas(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosRecebidosCelulas(int celula, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	/*-------------------------------------------BASES RECEBIDOS--------------------------------------------------------*/
+	List<Movimento> listarMovimentosRecebidosBases(int lider, int geracao, String recebido, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosRecebidosBases(int base, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosRecebidosBases(int base, String tipo, String especie, String recebido, String DataInicio, String DataFim);
+	
+	/*-------------------------------------------CELULAS ENVIADOS--------------------------------------------------------*/
+	List<Movimento> listarMovimentosEnviadosCelulas(int lider, int geracao, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosEnviadosCelulas(int celula, String tipo, String especie, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosEnviadosCelulas(int celula, String tipo, String especie, String DataInicio, String DataFim);
+	/*-------------------------------------------BASES ENVIADOS--------------------------------------------------------*/
+	List<Movimento> listarMovimentosEnviadosBases(int lider, String tipo, String especie, String DataInicio, String DataFim);
+	List<Object> totalMovimentosEnviadosBases(int base, String tipo, String especie, String DataInicio, String DataFim);
+	List<Movimento> listarMovimentosTodosEnviadosBases(int base, String tipo, String especie, String DataInicio, String DataFim);
 	
 	List<Movimento> listarMovimentoRelatorio(int movimento);
-	List<Movimento> listarMovimentosLembrarPrincipal(int discipulo);
+	List<Movimento> listarMovimentosLembrarCelulas(int discipulo);
+	List<Movimento> listarMovimentosLembrarBases(int discipulo);
 	
-	List<Object> trasCodigoDiscipuloRepasse(int movimento);
+	List<Object> trasCodigoDiscipuloRepassePorCelula(int movimento);
+	List<Object> trasCodigoDiscipuloRepassePorBase(int movimento);
 }
