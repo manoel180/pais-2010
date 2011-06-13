e385970a90ea26bd2c9c7be91d742260fca33a0c
andre.nascimento18@gmail.com <andre.nascimento18@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-06-13 19:39:48 +0000

    update

---
+++ trunk/Fonte/pais/src/br/com/pais/dao/impl/MinistraDaoImp.java
@@ -0,0 +1,8 @@
+package br.com.pais.dao.impl;
+
+import br.com.pais.dao.MinistraDao;
+import br.com.pais.entities.Ministra;
+
+public class MinistraDaoImp extends DaoGenericoImp<Ministra, Integer> implements MinistraDao{
+
+}
