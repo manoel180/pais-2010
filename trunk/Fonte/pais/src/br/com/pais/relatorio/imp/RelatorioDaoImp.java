59906f6c5e58115fc478e37ad5a80e7763411b9e
andre.nascimento18@gmail.com <andre.nascimento18@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-06-13 19:48:56 +0000

    upd

--- trunk/Fonte/pais/src/br/com/pais/relatorio/imp/RelatorioDaoImp.java
+++ trunk/Fonte/pais/src/br/com/pais/relatorio/imp/RelatorioDaoImp.java
@@ -7,20 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 import javax.faces.FacesException;
-import javax.faces.application.FacesMessage;
 import javax.faces.application.StateManager;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import br.com.pais.dao.impl.DiscipuloDaoImp;
-import br.com.pais.entities.Discipulos;
 import br.com.pais.relatorio.RelatorioDao;
 
 import com.mysql.jdbc.Connection;
@@ -36,30 +30,41 @@ public class RelatorioDaoImp implements RelatorioDao {
 		}
 		else{
 			jasper = getDiretorioReal("/jasper/protocolo/protocoloDinheiro.jasper");
-		}
-		String nomeRelatorioProtocolo = listaProtocolo.get(0).getProtocolo();
-        String path = getDiretorioReal("//relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
-		
-		FacesContext context = FacesContext.getCurrentInstance();
-	    HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+		}   
 		
 		try {
 			// Parametros do relatorios
 			Map<String, Object> parametros = new HashMap<String, Object>();
-			//parametros.put("SUBREPORT_DIR", getDiretorioReal("/jasper/") + "/");
 			
 			// Preenche a Lista
 			JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(listaProtocolo);
 			
-			//Mando o jasper gerar o relatorio. Nesse caso passo o map
-			JasperPrint impressao = JasperFillManager.fillReport(jasper, parametros, ds);
+			//Recebo o relatorio criado em memoria
+			byte[] pdf = JasperRunManager.runReportToPdf(jasper, parametros, ds);
+
+			//Captura uma instancia da página
+			FacesContext faces = FacesContext.getCurrentInstance();
 			
-			// Grava o relatorio em disco em pdf	
-			JasperManager.printReportToPdfFile(impressao, path);
-			// Redireciona para o pdf gerado
-			response.sendRedirect("/pais/relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
+			//Captura a respostas do contexto e set os tipo de retorno. 
+			HttpServletResponse response = (HttpServletResponse) faces.getExternalContext().getResponse();
+			response.setContentType("application/pdf");
+			response.setContentLength(pdf.length);
+			response.setHeader("Content-disposition", "inline");
+
+			response.setHeader("Cache-Control", "cache, must-revalidate");
+			response.setHeader("Pragma", "public");
+			ServletOutputStream out = response.getOutputStream();
+
+			out.write(pdf);
+			
+			StateManager stateManager = (StateManager) faces.getApplication().getStateManager();
+			stateManager.saveSerializedView(faces);
+			faces.responseComplete();
+		} catch (RuntimeException e) {
+			throw e;
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println(e.getMessage());
+			throw new FacesException(e);
 		}
 	}
 	
@@ -76,33 +81,40 @@ public class RelatorioDaoImp implements RelatorioDao {
 			pathSubRelatorio = getDiretorioReal("/jasper/protocolo/cheques.jasper");
 		}
 		
-		String nomeRelatorioProtocolo = listaProtocolo.get(0).getProtocolo();
-        String path = getDiretorioReal("//relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
-		
-		FacesContext context = FacesContext.getCurrentInstance();
-	    HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
-		
 		try {
 			// Parametros do relatorios
 			Map<String, Object> parametros = new HashMap<String, Object>();
 			parametros.put("SUBREPORT_DIR", pathSubRelatorio);
 			
-			//$P{SUBREPORT_DIR} + "cheques.jasper"
-			//new net.sf.jasperreports.engine.data.JRBeanCollectionDataSource($F{listacheques})
-			
 			// Preenche a Lista
 			JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(listaProtocolo);
 			
-			//Mando o jasper gerar o relatorio. Nesse caso passo o map
-			JasperPrint impressao = JasperFillManager.fillReport(jasper, parametros, ds);
+			//Recebo o relatorio criado em memoria
+			byte[] pdf = JasperRunManager.runReportToPdf(jasper, parametros, ds);
+
+			//Captura uma instancia da página
+			FacesContext faces = FacesContext.getCurrentInstance();
 			
-			// Grava o relatorio em disco em pdf	
-			JasperManager.printReportToPdfFile(impressao, path);
-			// Redireciona para o pdf gerado
-			response.sendRedirect("/pais/relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
+			//Captura a respostas do contexto e set os tipo de retorno. 
+			HttpServletResponse response = (HttpServletResponse) faces.getExternalContext().getResponse();
+			response.setContentType("application/pdf");
+			response.setContentLength(pdf.length);
+			response.setHeader("Content-disposition", "inline");
+
+			response.setHeader("Cache-Control", "cache, must-revalidate");
+			response.setHeader("Pragma", "public");
+			ServletOutputStream out = response.getOutputStream();
+
+			out.write(pdf);
+			
+			StateManager stateManager = (StateManager) faces.getApplication().getStateManager();
+			stateManager.saveSerializedView(faces);
+			faces.responseComplete();
+		} catch (RuntimeException e) {
+			throw e;
 		} catch (Exception e) {
-			e.printStackTrace();
-			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "ERRO!",e.getMessage()));
+			System.err.println(e.getMessage());
+			throw new FacesException(e);
 		}
 	}
 	
@@ -119,32 +131,40 @@ public class RelatorioDaoImp implements RelatorioDao {
 			pathSubRelatorio = getDiretorioReal("/jasper/protocolo/cheques.jasper");
 		}
 		
-		String nomeRelatorioProtocolo = listaProtocolo.get(0).getProtocolo();
-        String path = getDiretorioReal("//relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
-		
-		FacesContext context = FacesContext.getCurrentInstance();
-	    HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
-		
 		try {
 			// Parametros do relatorios
 			Map<String, Object> parametros = new HashMap<String, Object>();
 			parametros.put("SUBREPORT_DIR", pathSubRelatorio);
 			
-			//$P{SUBREPORT_DIR} + "cheques.jasper"
-			//new net.sf.jasperreports.engine.data.JRBeanCollectionDataSource($F{listacheques})
-			
 			// Preenche a Lista
 			JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(listaProtocolo);
 			
-			//Mando o jasper gerar o relatorio. Nesse caso passo o map
-			JasperPrint impressao = JasperFillManager.fillReport(jasper, parametros, ds);
+			//Recebo o relatorio criado em memoria
+			byte[] pdf = JasperRunManager.runReportToPdf(jasper, parametros, ds);
+
+			//Captura uma instancia da página
+			FacesContext faces = FacesContext.getCurrentInstance();
 			
-			// Grava o relatorio em disco em pdf	
-			JasperManager.printReportToPdfFile(impressao, path);
-			// Redireciona para o pdf gerado
-			response.sendRedirect("/pais/relatorio/protocolo/"+nomeRelatorioProtocolo.concat(".pdf")+"");
+			//Captura a respostas do contexto e set os tipo de retorno. 
+			HttpServletResponse response = (HttpServletResponse) faces.getExternalContext().getResponse();
+			response.setContentType("application/pdf");
+			response.setContentLength(pdf.length);
+			response.setHeader("Content-disposition", "inline");
+
+			response.setHeader("Cache-Control", "cache, must-revalidate");
+			response.setHeader("Pragma", "public");
+			ServletOutputStream out = response.getOutputStream();
+
+			out.write(pdf);
+			
+			StateManager stateManager = (StateManager) faces.getApplication().getStateManager();
+			stateManager.saveSerializedView(faces);
+			faces.responseComplete();
+		} catch (RuntimeException e) {
+			throw e;
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println(e.getMessage());
+			throw new FacesException(e);
 		}
 	}
 	
@@ -153,22 +173,18 @@ public class RelatorioDaoImp implements RelatorioDao {
 		return session.getServletContext().getRealPath(diretorio);
 	}
 
-	
 	@Override
-	public void gerarRelatorioGeracoes(List<Discipulos> discipulos) {
+	public void gerarRelatorioGeracoes(int Discipulador) {
 		String jasper = "";
 		String pathSubRelatorio = "";
 		jasper = getDiretorioReal("/jasper/geracoes/geracoesm12.jasper");
 		
-		// Preenche a Lista
-		JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(discipulos);
-		
 		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("logo", getDiretorioReal("/img/logoRelatorio_transparente.png"));
-		/*params.put("discipulador", Discipulador);*/
-		
+		params.put("logo", getDiretorioReal("/img/logoRelatorio.png"));
+		params.put("discipulador", Discipulador);
+
 		try {
-			gerarRelatorioPDF(jasper, params,ds);
+			gerarRelatorioPDF(jasper, params);
 		} catch (Exception e) {
 
 			System.err.println(e.getMessage());
@@ -186,14 +202,14 @@ public class RelatorioDaoImp implements RelatorioDao {
         return con;  
     }
 	
-	private void gerarRelatorioPDF(String nome, Map params,JRBeanCollectionDataSource ds) {
+	private void gerarRelatorioPDF(String nome, Map params) {
 
 		
 		try {
 
 			 
 			byte[] pdf = JasperRunManager.runReportToPdf(nome,
-					params,ds);
+					params,getConnection());
 
 			//Captura uma instancia da página
 			FacesContext faces = FacesContext.getCurrentInstance();
