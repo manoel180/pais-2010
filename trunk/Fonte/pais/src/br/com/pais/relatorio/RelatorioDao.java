0ec8db32d1276f3c882e8948335cd130ff4b1ae6
andre.nascimento18@gmail.com <andre.nascimento18@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-06-13 19:48:27 +0000

    add

---
+++ trunk/Fonte/pais/src/br/com/pais/relatorio/RelatorioDao.java
@@ -0,0 +1,12 @@
+package br.com.pais.relatorio;
+
+import java.util.List;
+
+import br.com.pais.relatorio.imp.Protocolo;
+
+public interface RelatorioDao{
+	void gerarProtocoloDinheiro(List<Protocolo> listaProtocolo);
+	void gerarProtocoloCheque(List<Protocolo> listaProtocolo);
+	void gerarProtocoloDinheiroCheque(List<Protocolo> listaProtocolo);
+	void gerarRelatorioGeracoes(int Discipulador);
+}
