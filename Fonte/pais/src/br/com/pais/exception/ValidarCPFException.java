4ee484030c0ac9e92b0f43243424f1c6aead7d60
andre.nascimento18@gmail.com <andre.nascimento18@gmail.com@f8513cf7-6285-0c83-a625-2453afe660e6>
2011-06-13 19:43:31 +0000

    upd

--- Fonte/pais/src/br/com/pais/exception/ValidarCPFException.java
+++ Fonte/pais/src/br/com/pais/exception/ValidarCPFException.java
@@ -1,17 +1,11 @@
 package br.com.pais.exception;
 
-
-
 public class ValidarCPFException extends Exception {
 
-	/**
-	 * 
-	 */
-//	private static final long serialVersionUID = 8275122096296509180L;
+	private static final long serialVersionUID = 1L;
 
 	public ValidarCPFException(String msg) {
 		super(msg);
-	
 	}
 
 	public ValidarCPFException(String msg, Throwable throwable) {
